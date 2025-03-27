c390f6daf84f23cf98b934efdffdac1e3e98cc9d
nieyo <jonasbucher95@gmail.com>
2025-03-27 16:14:20 +0100

    refactor: remove unnecessary lines

--- frontend/src/components/ShiftGallery.tsx
+++ frontend/src/components/ShiftGallery.tsx
@@ -4,29 +4,27 @@ import ShiftCard from "./ShiftCard.tsx";
 export default function ShiftGallery() {
 
     return (
-        <>
-            <Stack>
-                <Card>
-                    <Text>Schicht 1</Text>
-                    <Group>
-                        <ShiftCard/>
-                        <ShiftCard/>
-                        <ShiftCard/>
-                        <ShiftCard/>
-                        <ShiftCard/>
-                    </Group>
-                </Card>
-                <Card>
-                    <Text>Schicht 2</Text>
-                    <Group>
-                        <ShiftCard/>
-                        <ShiftCard/>
-                        <ShiftCard/>
-                        <ShiftCard/>
-                        <ShiftCard/>
-                    </Group>
-                </Card>
-            </Stack>
-        </>
+        <Stack>
+            <Card>
+                <Text>Schicht 1</Text>
+                <Group>
+                    <ShiftCard/>
+                    <ShiftCard/>
+                    <ShiftCard/>
+                    <ShiftCard/>
+                    <ShiftCard/>
+                </Group>
+            </Card>
+            <Card>
+                <Text>Schicht 2</Text>
+                <Group>
+                    <ShiftCard/>
+                    <ShiftCard/>
+                    <ShiftCard/>
+                    <ShiftCard/>
+                    <ShiftCard/>
+                </Group>
+            </Card>
+        </Stack>
     )
 }
