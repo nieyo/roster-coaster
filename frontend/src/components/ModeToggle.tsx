c438cce8a5694124b4edc33dad3a29586651532c
Jonas <jonasbucher95@gmail.com>
2025-05-10 02:14:38 +0200

    fix: theme import

--- frontend/src/components/ModeToggle.tsx
+++ frontend/src/components/ModeToggle.tsx
@@ -1,6 +1,6 @@
 import { Moon, Sun } from "lucide-react"
 import { Button } from "@/components/ui/button"
-import { useTheme } from "@/components/ThemeProvider.tsx"
+import {useTheme} from "@/context/theme-context.ts";
 
 export function ModeToggle() {
     const { theme, setTheme } = useTheme();
